47896d19e753cf3dda2c220b72d2367b23a4ce11
Benjamin Zaley <benjamin.zaley@wdc.com>
2019-05-30 15:41:25 -0500

    Remove C files

--- DNDThing/diceRoller/methods.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-char[] rollDice(int numRolls, int numSides); //Random num generator for dice of numSides
-char[] getRolls(int[] rolls); //Returns string of total num of rolls ("10 + 2 + ...")
-int getNumRolls(char[] dieRoll);
-int getNumSides(char[] dieRoll);
-//TODO
-//Average of all dice rolled
-//Roll multiple sets of dice, separated by spaces
