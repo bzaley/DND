80703c7252035e6519b85a4cb3c9ed3ec27f72f1
Benjamin Zaley <benjamin.zaley@wdc.com>
2019-06-05 10:46:39 -0500

    Commented out file reading for now, made function to perform D20 roll with advantage/disadvantage

--- DNDThing/diceRoller/rollD20.py
+++ DNDThing/diceRoller/rollD20.py
@@ -1,18 +1,25 @@
+import random
+
+def rollD20(modifier):
+        roll = random.randrange(1, 21)
+        print(roll)
+
 cont = 'y'
 while cont == 'y':
-    while True:
-    #Catch improper input and redirect user to try again
-        try:
-        filepath = raw_input("Give file path to character sheet: ")
-        charFile = open(filepath, "rt")
-        except ValueError:
-        print("File not found!")
-        continue
-        else:
-        #Proper input breaks from while true loop
-        break
-    print("File Found!")
-    pendingRoll = raw_input("Please enter the roll you would like to make: ")
+    # while True:
+    # #Catch improper input and redirect user to try again
+    #     try:
+    #     filepath = raw_input("Give file path to character sheet: ")
+    #     charFile = open(filepath, "rt")
+    #     except ValueError:
+    #     print("File not found!")
+    #     continue
+    #     else:
+    #     #Proper input breaks from while true loop
+    #     break
+    # print("File Found!")
+    #pendingRoll = raw_input("Please enter the roll you would like to make: ")
 
-    advDisadv = raw_input("Select if the roll is modified. [Na/Advantage/Disadvantage]: ")
-    
+    advDisadv = raw_input("Select if the roll is modified. [Na/Adv/Dis]: ")
+    advDisadv.lower()
+    rollD20(advDisadv)
